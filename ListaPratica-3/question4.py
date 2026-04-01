92e69e549eddb3c843bdbab3989d3ce00ee1ebed
xNeohaha <tuberanthony@gmail.com>
2026-04-01 10:42:40 -0300

    Exercicio 5

--- ListaPratica-3/question4.py
+++ ListaPratica-3/question4.py
@@ -1,3 +1,5 @@
 # Questão 4
 # Implemente o Merge Sort para ordenar uma lista encadeada simples. 
 # A função deve receber o nó cabeça da lista e retornar o nó cabeça da lista ordenada.
+
+# dificil?
--- ListaPratica-3/question5.py
+++ ListaPratica-3/question5.py
@@ -2,3 +2,32 @@
 # Crie uma classe Produto com atributos nome (string) e preco (float). 
 # Implemente o Quick Sort para ordenar uma lista de objetos Produto primeiramente por preco (crescente) 
 # e, em caso de preços iguais, por nome (alfabética crescente).
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+    def __repr__(self):
+        return f"({self.nome}: R${self.preco:.2f})"
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    
+    menores = [x for x in arr if x.preco < pivot.preco or (x.preco == pivot.preco and x.nome < pivot.nome)]
+    iguais = [x for x in arr if x.preco == pivot.preco and x.nome == pivot.nome]
+    maiores = [x for x in arr if x.preco > pivot.preco or (x.preco == pivot.preco and x.nome > pivot.nome)]
+
+    return quick_sort(menores) + iguais + quick_sort(maiores)
+
+
+Lista = [
+    Produto("Coxinha", 3.00),
+    Produto("Pepsi 500ML", 5.00),
+    Produto("Quibe", 3.00),
+    Produto("Pastel Bauru", 7.00),
+    Produto("Coca Cola 500ML", 5.50),
+    Produto("Enroladinho de Salsicha", 3.00)
+]
+LO = quick_sort(Lista)
+print("Original: ", Lista)
+print("Ordenada: ", LO)
